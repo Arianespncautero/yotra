c8e867d36b538e766f3bc4aaacc031e2bb54976f
Arianespncautero <105888302+Arianespncautero@users.noreply.github.com>
2024-10-08 10:45:17 -0300

    .

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-function mostraResposta (cartao_conteudo){
-    const resposta = cartao_conteudo.querySelector('.cartao_conteudo_resposta')
+function mostraResposta (cartao__conteudo){
+    const resposta = cartao__conteudo.querySelector('.cartao__conteudo__resposta')
     if (resposta.style.display == 'none'){
         resposta.style.display == 'block';
 
